695396dc74aa6cc9b40bfa38515b865e04ddcaf6
Calum <calumcmccall@gmail.com>
2022-06-21 17:08:51 +0100

    finished b6

--- exercise_b_users.py
+++ exercise_b_users.py
@@ -71,14 +71,14 @@ smallest = users["Erik"]["lottery_numbers"]
 smallest.sort()
 print(smallest[0])
 # 6. Return an list of Avril's lottery numbers that are even
-# numbers = users["Avril"]["lottery_numbers"]
-# even_numbers = []
+numbers = users["Avril"]["lottery_numbers"]
+even_numbers = []
 
-# for number in numbers:
-#   if number/2 == 0
+for number in numbers:
+  if number%2 == 0:
+    even_numbers.append(number)
 
-
-# print(even_numbers)
+print(even_numbers)
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
 # 8. Change Erik's hometown to Edinburgh
 # 9. Add a pet dog to Erik called "fluffy"
